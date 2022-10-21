ac8a4b8d8d1cd76ca969649f57dce08da94f7311
Alvin strandberg <Str4ndb3rg@live.se>
2022-10-21 09:32:26 +0100

    Add project files.

---
+++ Collection-generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Collection_generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
