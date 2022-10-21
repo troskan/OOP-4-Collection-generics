0357baa6892eb0ea7ff4a82b403d44d51d59c55e
Alvin strandberg <Str4ndb3rg@live.se>
2022-10-21 11:25:46 +0100

    new objects in main, created a constructor

---
+++ Collection-generics/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collection_generics
+{
+    class Employee
+    {
+        //ID, Name, Gender, Salary
+        public string Name {get; set;}
+        public int Id {get; set;}
+
+        public string Gender {get; set;}
+        public decimal Salary {get; set;}
+        public Employee(string name, int id, string gender, decimal salary)
+        {
+            this.Name = name;
+            this.Id = id;
+            this.Gender = gender;
+            this.Salary = salary;
+        }
+
+        Stack<string> personInfo = new Stack<string>
+        {
+
+        };
+    }
+}
--- Collection-generics/Program.cs
+++ Collection-generics/Program.cs
@@ -6,7 +6,13 @@ namespace Collection_generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee daniel = new Employee("Daniel", 239, "Man", 28.3999m);
+            Employee tobias = new Employee("Tobias", 183, "Man", 33.2450m);
+           
+            Employee sally = new Employee("Sally", 132, "Kvinna", 38.2350m);
+            Employee kim = new Employee("Kim", 215, "Kvinna", 32.8850m);
+
+
         }
     }
 }
